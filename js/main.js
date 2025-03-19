23314642651e7904f3cfed6426826a46532934bc
Josue <21387996@uagro.mx>
2025-03-18 18:51:52 -0600

    mejoras

--- js/main.js
+++ js/main.js
@@ -4,3 +4,17 @@ function palindromo (palabra){
 }
 
 console.log(palindromo('josue'))
+
+
+
+
+function palindromo_for (palabra){
+    palabra= palabra.toLowerCase()
+    invertido=''
+    for (let i=palabra.length -1; i>=0; i-- ){
+        invertido+=palabra[i];
+    }
+    return invertido===palabra;
+}
+
+console.log(palindromo_for('ana'))
